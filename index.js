7b4d229a745fb027f4432f33e4b38d523c096484
Martín Ciparelli <martin.ciparelli@55social.com>
2013-08-28 16:55:16 -0300

    fixed bug when trying to res.render, added support for multiple middlewares. Closes #1

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var co = require('co');
 var methods = require('methods');
-var wrapper = function (app) {
-	var routeWrapper = function (route) {
-		return function (path, generatorFunction) {
-			return route.bind(app)(path, function (req, res) {
-				co(generatorFunction, req, res);
-			});
-		};
-	}
+var createMiddleware = function (middlewares) {
+	return function (req, res) {
+		co(function* (req, res, middlewares) {
+			for (var i = 0, len = middlewares.length; i < len; i++) {
+				yield middlewares[i](req, res);
+			}
+		}, req, res, middlewares);
+	};
+}
+var routeWrapper = function (route, context) {
+	return function (path) {
+		var args = [path];
+		var middlewares = Array.prototype.slice.call(arguments, 1);
+		if (middlewares.length) {
+			args.push(createMiddleware(middlewares));
+		}
+		return route.apply(context, args);
+	};
+};
+module.exports = function (app) {
 	for (var i = 0; i < methods.length; i++) {
 		var method = methods[i];
-		app[method] = routeWrapper(app[method]);
+		app[method] = routeWrapper(app[method], app);
 	}
 	return app;
-}
-module.exports = wrapper;
+};
