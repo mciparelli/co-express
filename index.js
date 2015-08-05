a9fb70451c627d94ee1bdca46babb323330bf0a0
Blake Embrey <hello@blakeembrey.com>
2015-08-05 11:08:18 -0700

    Support error handlers

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var co = require('co');
 
 module.exports = function wrap(gen) {
   var fn = co.wrap(gen);
+
+  if (gen.length === 4) {
+    return function(err, req, res, next) {
+      return fn(err, req, res, next).catch(next);
+    }
+  }
+
   return function(req, res, next) {
     return fn(req, res, next).catch(next);
   };
